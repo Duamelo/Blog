be882e5c51bf9a4fd211935ac4d865f9758301b2
Duamelo <david.dosseh@imsp-uac.org>
2020-12-12 17:22:04 +0100

    validation progress authentication

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import {  ValidateInputPipe } from './core/pipes/validate.pipe';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //global prefix 
   app.setGlobalPrefix('api');
+
+  //handle all user input validation globally
+
+  //app.useGlobalPipes(new ValidateInputPipe());
+
+  
   await app.listen(2000);
 }
 bootstrap();
--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,8 +1,26 @@
+//import { IsNotEmpty, MinLength, IsEmail } from 'class-validator';
+
 export class UserDto {
+  //  @IsNotEmpty()
     readonly firstname: string;
+
+
+    //@IsNotEmpty()
     readonly lastname: string;
+    
+    
+    //@IsNotEmpty()
+    //@IsEmail()
     readonly email: string;
+    
+    
+    //@IsNotEmpty()
+    //@MinLength(6)
     readonly password: string;
+    
+    //@IsNotEmpty()
     readonly level: string;
+    
+    //@IsNotEmpty()
     readonly description: string;
 }
